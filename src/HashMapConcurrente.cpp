33b1f4c29e7caff0ad6f858e32f6961dc3d4272c
Fernando Gomez <fernando.gomez@mercadolibre.com>
2020-05-21 20:46:52 -0300

    alto codigo atr

--- src/HashMapConcurrente.cpp
+++ src/HashMapConcurrente.cpp
@@ -7,6 +7,8 @@
 
 #include "HashMapConcurrente.hpp"
 
+using namespace std;
+
 HashMapConcurrente::HashMapConcurrente() {
     for (unsigned int i = 0; i < HashMapConcurrente::cantLetras; i++) {
         tabla[i] = new ListaAtomica<hashMapPair>();
@@ -14,11 +16,11 @@ HashMapConcurrente::HashMapConcurrente() {
     }
 }
 
-unsigned int HashMapConcurrente::hashIndex(std::string clave) {
+unsigned int HashMapConcurrente::hashIndex(string clave) {
     return (unsigned int)(clave[0] - 'a');
 }
 
-void HashMapConcurrente::incrementar(std::string clave) {
+void HashMapConcurrente::incrementar(string clave) {
     // Completar (Ejercicio 2)
     unsigned int index = hashIndex(clave);
 
@@ -27,13 +29,13 @@ void HashMapConcurrente::incrementar(std::string clave) {
     if (indiceLista != -1) {
         tabla[index]->iesimo(indiceLista).second += 1;
     } else {
-        hashMapPair valor = std::make_pair(clave, 1);
+        hashMapPair valor = make_pair(clave, 1);
         tabla[index]->insertar(valor);
     }
     pthread_mutex_unlock(&mutexTabla[index]);
 }
 
-int HashMapConcurrente::buscarIndice(std::string clave) {
+int HashMapConcurrente::buscarIndice(string clave) {
     // Completar (Ejercicio 2)
     unsigned int index = hashIndex(clave);
     ListaAtomica<hashMapPair>::Iterador it = tabla[index]->crearIt();
@@ -49,9 +51,9 @@ int HashMapConcurrente::buscarIndice(std::string clave) {
     return -1;
 }
 
-std::vector<std::string> HashMapConcurrente::claves() {
+vector<string> HashMapConcurrente::claves() {
     // Completar (Ejercicio 2)
-    std::vector<std::string> claves;
+    vector<string> claves;
 
     for (int index = 0; index < cantLetras; index++) {
         pthread_mutex_lock(&mutexTabla[index]);
@@ -67,7 +69,7 @@ std::vector<std::string> HashMapConcurrente::claves() {
     return claves;
 }
 
-unsigned int HashMapConcurrente::valor(std::string clave) {
+unsigned int HashMapConcurrente::valor(string clave) {
     // Completar (Ejercicio 2)
     unsigned int result = 0;
     unsigned int index = hashIndex(clave);
@@ -87,11 +89,11 @@ hashMapPair HashMapConcurrente::maximo() {
     max->second = 0;
 
     for (unsigned int index = 0; index < HashMapConcurrente::cantLetras; index++) {
-        for (
-            auto it = tabla[index]->crearIt();
-            it.haySiguiente();
-            it.avanzar()
-        ) {
+        pthread_mutex_lock(&mutexTabla[index]); 
+    }
+
+    for (unsigned int index = 0; index < HashMapConcurrente::cantLetras; index++) {
+        for (auto it = tabla[index]->crearIt(); it.haySiguiente(); it.avanzar()) {
             if (it.siguiente().second > max->second) {
                 max->first = it.siguiente().first;
                 max->second = it.siguiente().second;
@@ -99,11 +101,83 @@ hashMapPair HashMapConcurrente::maximo() {
         }
     }
 
+    for (unsigned int index = 0; index < HashMapConcurrente::cantLetras; index++) {
+        pthread_mutex_unlock(&mutexTabla[index]);
+    }
+
     return *max;
+}
+
+struct ThreadMaximoData {
+    ListaAtomica<hashMapPair>* *tabla;
+    atomic<hashMapPair*> *maximo;
+    atomic_flag *filasProcesadas;
+    unsigned int cantFilas;
+};
+
+void *threadMaximo(void *args) {
+    ThreadMaximoData params = *(ThreadMaximoData*) args;
+
+    for (unsigned int index = 0; index < params.cantFilas; index++) {
+        if (!params.filasProcesadas[index].test_and_set()) {
+
+            hashMapPair* maximoFila = new hashMapPair("", 0);
+
+            for (auto it = params.tabla[index]->crearIt(); it.haySiguiente(); it.avanzar()) {
+                if (it.siguiente().second > maximoFila->second) {
+                    maximoFila->first = it.siguiente().first;
+                    maximoFila->second = it.siguiente().second;
+                }
+            }
+
+            while (true) {
+                hashMapPair* maximoActual = params.maximo->load();
+                if (maximoFila->second > maximoActual->second && params.maximo->compare_exchange_strong(maximoActual, maximoFila)) {
+                    break;
+                }
+                if (maximoFila->second <= maximoActual->second) {
+                    break;
+                }
+            }
+        }
+    }
+
+    pthread_exit(0);
 }
 
 hashMapPair HashMapConcurrente::maximoParalelo(unsigned int cantThreads) {
     // Completar (Ejercicio 3)
+    atomic<hashMapPair*> maximo;
+    maximo.store(new hashMapPair("", 0));
+
+    atomic_flag filasProcesadas[cantLetras] = {ATOMIC_FLAG_INIT};
+
+    pthread_t threads[cantThreads];
+    ThreadMaximoData threadData[cantThreads];
+   
+    for (unsigned int index = 0; index < cantLetras; index++) {
+        pthread_mutex_lock(&mutexTabla[index]); 
+    }
+
+    for (unsigned int index = 0; index < cantThreads; index++) {
+        threadData[index].tabla = tabla;
+        threadData[index].maximo = &maximo;
+        threadData[index].filasProcesadas = filasProcesadas;
+        threadData[index].cantFilas = cantLetras;
+
+        pthread_create(&threads[index], NULL, threadMaximo, &threadData[index]);
+    }
+
+    for (unsigned int index = 0; index < cantThreads; index++) {
+        pthread_join(threads[index], NULL);
+    }
+
+    for (unsigned int index = 0; index < cantLetras; index++) {
+        pthread_mutex_unlock(&mutexTabla[index]);
+    }
+
+    hashMapPair* maxValue = maximo.load();
+    return hashMapPair(maxValue->first, maxValue->second);
 }
 
 #endif
