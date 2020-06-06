78cadefeb33d99d74630392c021831a3c1b4608e
Fernando Gomez <fernando.gomez@mercadolibre.com>
2020-06-06 15:09:39 -0300

    tests

--- src/Experimentos.cpp
+++ src/Experimentos.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 
 double loadFilesTime(vector<string> filePaths, int cantThreads);
+double maximoParaleloTime(vector<string> filePaths, int cantThreads);
 
 int main() {
 
@@ -25,12 +26,21 @@ int main() {
     filePaths.push_back("data/wet-7");
     filePaths.push_back("data/wet-8");
     filePaths.push_back("data/wet-9");
+    filePaths.push_back("data/wet-10");
 
-    for (int i = 1; i <= filePaths.size(); i++) {
+    /*for (int i = 1; i <= filePaths.size() * 2; i++) {
         double time = loadFilesTime(filePaths, i);
         printf("Escenario 1 con %d threads: %lf\n", i, time);
-    }
-    
+    }*/
+
+
+    // Escenario 2 - Calculo de valor Maximo en paralelo
+    /*int cantThreads = 50;
+    for (int i = 1; i <= cantThreads; i++) {
+        double time = maximoParaleloTime(filePaths, i);
+        printf("%lf\n", time);
+    }*/
+
     return(EXIT_SUCCESS);
 }
 
@@ -48,6 +58,36 @@ double loadFilesTime(vector<string> filePaths, int cantThreads) {
 
         HashMapConcurrente hashMap = HashMapConcurrente();
         cargarMultiplesArchivos(hashMap, cantThreads, filePaths);
+
+        if (clock_gettime(CLOCK_REALTIME, &stop) == -1) {
+            perror("clock gettime stop");
+            exit(EXIT_FAILURE);
+        }
+
+        double accum = (double)(stop.tv_sec - start.tv_sec) * (double)1000 + (double)(stop.tv_nsec - start.tv_nsec) / 1000000;
+        if (accum < minTime) {
+            minTime = accum;
+        }   
+    }
+
+    return minTime;
+}
+
+double maximoParaleloTime(vector<string> filePaths, int cantThreads) {
+    HashMapConcurrente hmc = HashMapConcurrente();
+    cargarMultiplesArchivos(hmc, 4, filePaths);
+    double minTime = 99999999999999;
+    int iteraciones = 10;
+
+    for (int i = 0; i < iteraciones; i++) {
+        struct timespec start, stop;
+
+        if (clock_gettime(CLOCK_REALTIME, &start) == -1) {
+            perror("clock gettime start");
+            exit(EXIT_FAILURE);
+        }
+
+        hmc.maximoParalelo(cantThreads);
 
         if (clock_gettime(CLOCK_REALTIME, &stop) == -1) {
             perror("clock gettime stop");
